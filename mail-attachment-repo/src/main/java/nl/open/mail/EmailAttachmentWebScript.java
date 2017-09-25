78a9e7ffd8e3aac2dd3675802f3e6a979efdab23
Mark Tielemans <mark.tielemans@open.nl>
2017-09-26 01:57:23 +0200

    Fixed: Incomplete request now properly throws a HTTP 400. Added: Test webscript form.

--- mail-attachment-repo/src/main/java/nl/open/mail/EmailAttachmentWebScript.java
+++ mail-attachment-repo/src/main/java/nl/open/mail/EmailAttachmentWebScript.java
@@ -16,57 +16,62 @@ import org.springframework.util.Assert;
 
 public class EmailAttachmentWebScript extends DeclarativeWebScript {
 
-	private static final String PARAM_ATTACHMENTS = "attachments";
-	private static final String PARAM_RECIPIENTS = "recipients";
-	private static final String PARAM_SUBJECT = "subject";
-	private static final String PARAM_TEMPLATE = "template";
-	
-	private static final Logger logger = LogManager.getLogger(EmailAttachmentWebScript.class);
-	
-	public final void setEmailWithAttachmentsService(EMailWithAttachmentsService emailWithAttachmentsService) {
+    private static final String PARAM_ATTACHMENTS = "attachments";
+    private static final String PARAM_RECIPIENTS = "recipients";
+    private static final String PARAM_SUBJECT = "subject";
+    private static final String PARAM_TEMPLATE = "template";
+
+    private static final Logger logger = LogManager.getLogger(EmailAttachmentWebScript.class);
+
+    public final void setEmailWithAttachmentsService(EMailWithAttachmentsService emailWithAttachmentsService) {
         this.emailWithAttachmentsService = emailWithAttachmentsService;
     }
 
     private EMailWithAttachmentsService emailWithAttachmentsService;
-	
-	protected NodeRef[] asNodeRefs(final String... refs) {
-		final NodeRef[] nodeRefs = new NodeRef[refs.length];
-		for (int i = 0; i < refs.length; i++) {
-			nodeRefs[i] = new NodeRef(refs[i]);
-		}
-		
-		return nodeRefs;
-	}
-	
-	@Override
-	protected void executeFinallyImpl(final WebScriptRequest req, final Status status, 
-			final Cache cache, final Map<String, Object> model) {
 
-	    checkAvailability(req, PARAM_ATTACHMENTS, PARAM_RECIPIENTS);
-	    
-		final EmailWithAttachmentsTask task = new EmailWithAttachmentsTask();
-		task.setAttachments(asNodeRefs(req.getParameterValues(PARAM_ATTACHMENTS)));
-		task.setRecipients(req.getParameterValues(PARAM_RECIPIENTS));
-		task.setSubject(req.getParameter(PARAM_SUBJECT));
-		
-		if (StringUtils.isNotBlank(req.getParameter(PARAM_TEMPLATE))) {
-			task.setTemplate(new NodeRef(req.getParameter(PARAM_TEMPLATE)));
-		}
-		
-		try {
-			emailWithAttachmentsService.sendEmail(task);
-		} catch (final IOException e) {
-			logger.error("Failed to send email with attachments", e);
-			status.setException(e);
-			status.setCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
-			return;
-		}
-	}
-	
-	private void checkAvailability(final WebScriptRequest req, final String... paramKeys) {
-	    for (final String paramKey : paramKeys) {
-	        Assert.hasText(req.getParameter(paramKey), 
-	                "Parameter " + paramKey + " is mandatory!");
-	    }
-	}
+    protected NodeRef[] asNodeRefs(final String... refs) {
+        final NodeRef[] nodeRefs = new NodeRef[refs.length];
+        for (int i = 0; i < refs.length; i++) {
+            nodeRefs[i] = new NodeRef(refs[i]);
+        }
+
+        return nodeRefs;
+    }
+    
+    private void checkAvailability(final WebScriptRequest req, final Status status, final String... paramKeys) {
+        try {
+            for (final String paramKey : paramKeys) {
+                Assert.hasText(req.getParameter(paramKey), 
+                        "Parameter " + paramKey + " is mandatory!");
+            }
+        } catch (final IllegalArgumentException iae) {
+            status.setCode(HttpStatus.SC_BAD_REQUEST);
+            throw iae;
+        }
+    }
+
+    @Override
+    protected void executeFinallyImpl(final WebScriptRequest req, final Status status, 
+            final Cache cache, final Map<String, Object> model) {
+
+        checkAvailability(req, status, PARAM_ATTACHMENTS, PARAM_RECIPIENTS);
+
+        final EmailWithAttachmentsTask task = new EmailWithAttachmentsTask();
+        task.setAttachments(asNodeRefs(req.getParameterValues(PARAM_ATTACHMENTS)));
+        task.setRecipients(req.getParameterValues(PARAM_RECIPIENTS));
+        task.setSubject(req.getParameter(PARAM_SUBJECT));
+
+        if (StringUtils.isNotBlank(req.getParameter(PARAM_TEMPLATE))) {
+            task.setTemplate(new NodeRef(req.getParameter(PARAM_TEMPLATE)));
+        }
+
+        try {
+            emailWithAttachmentsService.sendEmail(task);
+        } catch (final IOException e) {
+            logger.error("Failed to send email with attachments", e);
+            status.setException(e);
+            status.setCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+    }
 }
---
+++ mail-attachment-repo/src/main/resources/alfresco/extension/templates/webscripts/nl/open/send-mail-attachment.get.html.ftl
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Test mail with attachments</title>
+</head>
+
+<body>
+  <form method="POST">
+  
+    <label for="recipients">To:</label>&nbsp;&nbsp;
+    <input type="text" name="recipients" />
+    <br /><br />
+    <label for="attachments">Attachments:</label>&nbsp;&nbsp;
+    <input type="text" name="attachments" />
+    <br /><br />
+    <label for="subject">Subject:</label>&nbsp;&nbsp;
+    <input type="text" name="subject" />
+    <br /><br />
+    <input type="submit" />
+  </form>
+</body>
+</html>
